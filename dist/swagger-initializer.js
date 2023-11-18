026c95d0d2f3babdd768640adcf209cb7162b1d2
gokul-1998 <82328858+gokul-1998@users.noreply.github.com>
2023-11-18 08:42:47 +0530

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: "https://sparkling-selkie-322987.netlify.app/API_team_11.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
